25ca9cb7263f1a5ca00bcbbcac1cd6ea219afb52
LillLEE <kopqwe147@gmail.com>
2021-10-13 23:10:02 +0900

    수정렬하기

---
+++ Algorithm/BackJoon/수정렬하기/Main.java
@@ -0,0 +1,21 @@
+package BackJoon.수정렬하기;
+
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		int[] nums;
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		nums = new int[n];
+		for (int i = 0; i < n; i++) {
+			nums[i] = scan.nextInt();
+		}
+		Arrays.sort(nums);
+		for (int num : nums) {
+			System.out.println(num);
+		}
+		scan.close();
+	}
+}

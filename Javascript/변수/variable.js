b930cb47eff68b1171a2b2f37951c36091b4da64
LillLEE <kopqwe147@gmail.com>
2021-11-20 23:15:38 +0900

    ADD JS 변수

---
+++ Javascript/변수/variable.js
@@ -0,0 +1,22 @@
+let name = "LEE";
+let age = 26;
+
+console.log(name);
+
+// 변수는 문자와 숫자 , $와 _만 사용
+const MY_HOME = "...";
+let _ = 1;
+let $ = 3;
+
+// 첫글자는 숫자가 될 수 없다.
+//let 1stGrade = 'A+' 안됨
+
+// 예약어는 사용할 수 없다.
+let let = 99;
+
+// 가급적 상수는 대문자로
+const MAX_SIZE = 99;
+
+// 변수명은 읽기 쉽고 이해할 수 있게 선언
+let a = 1; // X
+let userNumber = 3; // O
